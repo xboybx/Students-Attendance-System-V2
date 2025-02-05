eb843881daa5cd6545da06f083047283832ae999
Jaswanth <jeswanth012@gmail.com>
2025-02-05 12:06:27 +0530

    removed Network Url

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: "/Students-Attendance-System-V2",
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    open: true
-  },
+  // server: {
+  //   port: 5173,
+  //   host: true,
+  //   open: true
+  // },
   preview: {
     port: 5173,
     host: true,
