2dfcf26728b7a1d09695b079f9d43841a5996f0e
Jaswanth <jeswanth012@gmail.com>
2025-02-05 12:19:51 +0530

    kgrd

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Students-Attendance-System-V2",
+  // base: "/Students-Attendance-System-V2",
   plugins: [react()],
-  // server: {
-  //   port: 5173,
-  //   host: true,
-  //   open: true
-  // },
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  },
   preview: {
     port: 5173,
     host: true,
